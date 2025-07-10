38477e8f25d964efe2e2e6c3086b8aa02a6eef68
Ivan Gordillo <gordillosolis@icloud.com>
2025-07-10 02:08:11 -0600

    Add CSV and Serde dependencies; implement result saving functionality

--- rutas_vehiculares/src/main.rs
+++ rutas_vehiculares/src/main.rs
@@ -2,7 +2,21 @@ mod rutas_vehiculares;
 
 use crate::rutas_vehiculares::*;
 
+use csv::Writer;
+use serde::Serialize;
 use std::env;
+use std::fs::create_dir_all;
+use std::path::Path;
+
+#[derive(Serialize)]
+struct Fila {
+    ejec: usize,
+    mas_importancia_distancia: usize,
+    solo_distancia: usize,
+    solo_feromona: usize,
+    mas_importancia_feromona: usize,
+    importancia_ambas: usize,
+}
 
 fn rutas_vehiculares(
     n_hormigas: usize,
@@ -11,7 +25,8 @@ fn rutas_vehiculares(
     importancia_distancia: f64,
     importancia_feromona: f64,
     filepath: String,
-) {
+    test: bool,
+) -> usize {
     let datos = leer_matriz(filepath);
 
     // Inicializacion de conjunto de aristas, feromonas y distancias
@@ -26,8 +41,10 @@ fn rutas_vehiculares(
     };
 
     for i in 1..=max_iteraciones {
-        print!("\rProgreso: {}/{}", i, max_iteraciones);
-        std::io::Write::flush(&mut std::io::stdout()).unwrap();
+        if !test {
+            print!("\rProgreso: {}/{}", i, max_iteraciones);
+            std::io::Write::flush(&mut std::io::stdout()).unwrap();
+        }
 
         let mut hormigas = inicializacion_hormigas(n_hormigas);
 
@@ -66,21 +83,25 @@ fn rutas_vehiculares(
 
         actualizacion_feromona(&hormigas, &mut cf, &evaluacion_rutas);
     }
-    println!();
-    println!("Mejor solución encontrada:");
-    println!("Costo total: {}", mejor_solucion.costo_total);
-    println!("Vehículos usados: {}", mejor_solucion.vehiculos_usados);
-    for (i, ruta) in mejor_solucion.rutas.iter().enumerate() {
-        println!("Ruta {}: {:?}", i + 1, ruta);
+
+    if !test {
+        println!();
+        println!("Mejor solución encontrada:");
+        println!("Costo total: {}", mejor_solucion.costo_total);
+        println!("Vehículos usados: {}", mejor_solucion.vehiculos_usados);
+        for (i, ruta) in mejor_solucion.rutas.iter().enumerate() {
+            println!("Ruta {}: {:?}", i + 1, ruta);
+        }
+        println!();
     }
-    println!();
-    println!("Valor optimo encontrado: {}", mejor_solucion.costo_total);
+    // println!("Valor optimo encontrado: {}", mejor_solucion.costo_total);
+    mejor_solucion.costo_total as usize
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 6 {
-        eprintln!("Uso: {} <n_hormigas> <rho> <max_iteraciones> <importancia_distancia> <importancia_feromona> <filepath>", args[0]);
+    if args.len() < 7 {
+        eprintln!("Uso: {} <n_hormigas> <rho> <max_iteraciones> <importancia_distancia> <importancia_feromona> <filepath> <test>", args[0]);
         return;
     }
     let n_hormigas: usize = args[1]
@@ -97,6 +118,7 @@ fn main() {
         .parse()
         .expect("Error al parsear la importancia de la feromona");
     let filepath = args[6].clone();
+    let test = args.get(7).map_or(false, |s| s == "test");
 
     println!("Ejecutando algoritmo con los siguientes parámetros:");
     println!("Número de hormigas: {}", n_hormigas);
@@ -107,16 +129,84 @@ fn main() {
     println!("Archivo de datos: {}", filepath);
     println!();
 
-    rutas_vehiculares(
-        n_hormigas,
-        rho,
-        max_iteraciones,
-        importancia_distancia,
-        importancia_feromona,
-        filepath.clone(),
-    );
+    if test {
+        let dir = "resultados";
+        create_dir_all(dir).expect("No se pudo crear la carpeta de resultados");
 
-    if let Some(valor_optimo) = leer_valor_optimo(&filepath) {
-        println!("Valor óptimo conocido: {}", valor_optimo);
+        let file_stem = Path::new(&filepath)
+            .file_stem()
+            .and_then(|s| s.to_str())
+            .unwrap_or("output");
+
+        let csv_path = format!("{}/{}.csv", dir, file_stem);
+        println!("Guardando resultados en: {}", csv_path);
+
+        let mut writer = Writer::from_path(&csv_path).expect("No se pudo crear el archivo CSV");
+
+        for i in 0..30 {
+            let mut resultados = vec![0; 5];
+            for j in 0..=4 {
+                print!(
+                    "\rEjecucion {}: {}/30, Prueba de parametros: {}/5",
+                    &filepath,
+                    i + 1,
+                    j + 1
+                );
+                std::io::Write::flush(&mut std::io::stdout()).unwrap();
+
+                let mut i_d = 0.0;
+                let mut i_f = 0.0;
+                if j == 0 {
+                    i_d = 4.0;
+                    i_f = 1.0;
+                } else if j == 1 {
+                    i_d = 1.0;
+                    i_f = 0.0;
+                } else if j == 2 {
+                    i_d = 0.0;
+                    i_f = 1.0;
+                } else if j == 3 {
+                    i_d = 1.0;
+                    i_f = 4.0;
+                } else if j == 4 {
+                    i_d = 1.0;
+                    i_f = 1.0;
+                }
+                resultados[j] = rutas_vehiculares(
+                    n_hormigas,
+                    rho,
+                    max_iteraciones,
+                    i_d,
+                    i_f,
+                    filepath.clone(),
+                    test,
+                );
+            }
+            let fila = Fila {
+                ejec: i + 1,
+                mas_importancia_distancia: resultados[0],
+                solo_distancia: resultados[1],
+                solo_feromona: resultados[2],
+                mas_importancia_feromona: resultados[3],
+                importancia_ambas: resultados[4],
+            };
+            writer.serialize(fila).expect("No se pudo escribir la fila");
+        }
+        println!();
+        writer.flush().expect("No se pudo guardar el archivo CSV");
+    } else {
+        let mejor_costo = rutas_vehiculares(
+            n_hormigas,
+            rho,
+            max_iteraciones,
+            importancia_distancia,
+            importancia_feromona,
+            filepath.clone(),
+            test,
+        );
+        if let Some(valor_optimo) = leer_valor_optimo(&filepath) {
+            println!("Valor óptimo conocido: {}", valor_optimo);
+        }
+        println!("Costo de la mejor solución: {}", mejor_costo);
     }
 }
